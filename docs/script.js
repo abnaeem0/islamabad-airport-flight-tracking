683ebf170090ec964de7121a17f68080df85ddf6
abnaeem0 <abubakarnaeem0@gmail.com>
2026-02-16 11:12:38 +0500

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -91,3 +91,5 @@ document.addEventListener('DOMContentLoaded', () => {
  window.viewHistory = function(flightNumber, date) {
     window.location.href = `flight_detail.html?flight=${flightNumber}&date=${date}`;
   };
+  
+});
