9645ba92bb8813c70a76b44aba1e058d61d6834f
abnaeem0 <abubakarnaeem0@gmail.com>
2026-02-16 10:37:13 +0500

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const supabaseUrl = 'https://sbaweaytsmdmhaclgcwr.supabase.co';
   const supabaseKey = 'sb_publishable_PBY7Y_HM60Ijqw9j6iOGeg_XqLDI7SS';
-  window.client = supabase.createClient(supabaseUrl, supabaseKey);
+  const client = supabase.createClient(supabaseUrl, supabaseKey);
 
   // DOM elements
   const searchBtn = document.getElementById('search-btn');
